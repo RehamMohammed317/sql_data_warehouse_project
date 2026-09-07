dfc6474ef6ab89bbf316ab0aaa33233f5d1b14bf
Reham Mohammed Darwish <Reham.darwish2002@gmail.com>
2026-09-07 20:57:06 +0300

    Delete scripts/silver directory

--- scripts/silver/check bronze.erp_cust_az12.sql
+++
@@ -1,9 +0,0 @@
---check identify out-of-range dates 
-select distinct 
-bdate 
-from bronze.erp_cust_az12
-where bdate <'1924-01-01' OR bdate >getDate()
-
---data standardization & consistency
-select distinct gen
-from bronze.erp_cust_az12
